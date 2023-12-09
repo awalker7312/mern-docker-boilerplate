8c79c4933e86b7acdd8042f456f04ec45b8296a0
Alan Walker <alanwalk3r@gmail.com>
2023-12-09 13:20:44 -0500

    Refactor CustomSnackbar component

--- frontend/src/components/CustomSnackbar.jsx
+++ frontend/src/components/CustomSnackbar.jsx
@@ -1,34 +1,12 @@
 // CustomSnackbar.jsx
-import React from 'react';
 import Snackbar from '@mui/material/Snackbar';
 import Alert from '@mui/material/Alert';
 import PropTypes from 'prop-types';
 
 const CustomSnackbar = ({open, handleClose, message, severity}) => {
-	const [isOpen, setIsOpen] = React.useState(open);
-
-	React.useEffect(() => {
-		setIsOpen(open);
-	}, [open]);
-
-	const handleCloseSnackbar = (event, reason) => {
-		if (reason === 'clickaway') {
-			return;
-		}
-
-		setIsOpen(false);
-		handleClose();
-	};
-
 	return (
-		<Snackbar
-			open={isOpen}
-			autoHideDuration={6000}
-			onClose={handleCloseSnackbar}>
-			<Alert
-				onClose={handleCloseSnackbar}
-				severity={severity}
-				sx={{width: '100%'}}>
+		<Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+			<Alert onClose={handleClose} severity={severity} sx={{width: '100%'}}>
 				{message}
 			</Alert>
 		</Snackbar>
